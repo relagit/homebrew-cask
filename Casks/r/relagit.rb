77a71c876339b52539aa3519d3e687b970979940
axolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-03-26 07:51:08 +1100

    add relagit cask

---
+++ Casks/r/relagit.rb
@@ -0,0 +1,25 @@
+cask "relagit" do
+  version "0.12.12"
+  sha256 arm:   "78434530e51e1208923703cb43504b01a39a121d5a084debff5566dd5b953acd",
+         intel: "a0f370116f8b30d697b9c205572549d93bb3cfa2bc0d97ff18106cf0f54bc0c4"
+  arch arm: "arm64", intel: "x64"
+
+  url "https://github.com/relagit/relagit/releases/download/v#{version}/RelaGit-mac-#{arm}.dmg",
+      verified: "github.com/relagit/relagit/"
+  name "RelaGit"
+  desc "The elegant solution to graphical version control."
+  homepage "https://git.rela.dev/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "RelaGit.app"
+
+  zap trash: [
+    "~/Library/Application Support/RelaGit",
+    "~/Library/Logs/RelaGit",
+    "~/.relagit",
+  ]
+end
