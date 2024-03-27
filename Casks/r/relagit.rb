790bc128ecbc7b6bc36d61455d020061d35a0383
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-03-27 19:48:53 +1100

    relagit: run brew style

--- Casks/r/relagit.rb
+++ Casks/r/relagit.rb
@@ -2,13 +2,13 @@ cask "relagit" do
   arch arm: "arm64", intel: "x64"
 
   version "0.12.12"
-  sha256 arm: "78434530e51e1208923703cb43504b01a39a121d5a084debff5566dd5b953acd",
+  sha256 arm:   "78434530e51e1208923703cb43504b01a39a121d5a084debff5566dd5b953acd",
          intel: "a0f370116f8b30d697b9c205572549d93bb3cfa2bc0d97ff18106cf0f54bc0c4"
 
   url "https://github.com/relagit/relagit/releases/download/v#{version}/RelaGit-mac-#{arm}.dmg",
       verified: "github.com/relagit/relagit/"
   name "RelaGit"
-  desc "The elegant solution to graphical version control."
+  desc "Elegant solution to graphical version control"
   homepage "https://git.rela.dev/"
 
   livecheck do
@@ -19,8 +19,8 @@ cask "relagit" do
   app "RelaGit.app"
 
   zap trash: [
+    "~/.relagit",
     "~/Library/Application Support/RelaGit",
     "~/Library/Logs/RelaGit",
-    "~/.relagit",
   ]
 end
