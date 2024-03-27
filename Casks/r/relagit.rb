63d8d7189191ec350eb50f94851841ac7b378b70
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-03-27 19:51:16 +1100

    relagit: fix download url

--- Casks/r/relagit.rb
+++ Casks/r/relagit.rb
@@ -5,7 +5,7 @@ cask "relagit" do
   sha256 arm:   "78434530e51e1208923703cb43504b01a39a121d5a084debff5566dd5b953acd",
          intel: "a0f370116f8b30d697b9c205572549d93bb3cfa2bc0d97ff18106cf0f54bc0c4"
 
-  url "https://github.com/relagit/relagit/releases/download/v#{version}/RelaGit-mac-#{arm}.dmg",
+  url "https://github.com/relagit/relagit/releases/download/v#{version}/RelaGit-mac-#{arch}.dmg",
       verified: "github.com/relagit/relagit/"
   name "RelaGit"
   desc "Elegant solution to graphical version control"
